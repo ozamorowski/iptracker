1176745fdd4a51f13d26e68de67920bd83c6f237
Oskar Zamorowski <ozamorowski@gmail.com>
2022-06-07 18:59:29 +0200

    Header with searh input markup.'

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <img class="m-auto" alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <header class="pt-6">
+    <div class="container mx-auto">
+      <h1 class="text-white text-3xl">IP Address Tracker</h1>
+      <input
+        type="text"
+        class="mt-6 px-6 py-3 w-[35%] rounded-l-2xl focus:shadow focus:outline-none"
+        placeholder="Search any IP address or domain"
+      />
+      <button class="bg-black text-white rounded-r-2xl px-5 py-3 font-bold">
+        >
+      </button>
+    </div>
+  </header>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Rubik', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+header {
+  min-height: 250px;
+  background-image: url('assets/pattern-bg.png');
 }
 </style>
