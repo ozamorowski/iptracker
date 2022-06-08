2b502023ea7f86f8f88dabeb0fd5e14cecc729ea
Oskar Zamorowski <ozamorowski@gmail.com>
2022-06-08 17:36:46 +0200

    IP info section, minor styling, error catching.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,132 @@
 <script setup lang="ts">
-import Map from './components/Map.vue'
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+import { onMounted, ref } from 'vue'
+
+let map: L.Map
+let inputQuery = ref('')
+let loading = ref(false)
+let error = ref(false)
+let ipInfo = ref({
+  ip: '',
+  isp: '',
+  timezone: '',
+  city: '',
+  region: '',
+  country: '',
+})
+
+const getIpInfo = async (): Promise<void> => {
+  loading.value = true
+  error.value = false
+
+  const baseUrl: string =
+    'https://geo.ipify.org/api/v2/country,city?apiKey=at_oC6vXlb4vmhsNipQcHGi24XZq6Kbf'
+
+  const url: string =
+    baseUrl + (inputQuery.value ? '&domain=' + inputQuery.value : '')
+
+  try {
+    const request = await fetch(url)
+    const response = await request.json()
+
+    if (response.code) {
+      error.value = true
+      return
+    }
+
+    const {
+      ip,
+      isp,
+      location: { lat, lng, timezone, city, region, country },
+    } = response
+
+    ipInfo.value = { ip, isp, timezone, city, region, country }
+
+    L.marker([lat, lng]).addTo(map)
+    map.setView([lat, lng], 13)
+
+    loading.value = false
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+onMounted(() => {
+  map = L.map('map')
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap',
+  }).addTo(map)
+
+  getIpInfo()
+})
 </script>
 
 <template>
-  <header class="pt-6">
+  <header class="pt-6 max-h-64">
     <div class="container mx-auto">
-      <h1 class="text-white text-3xl">IP Address Tracker</h1>
+      <h1 class="text-white text-3xl font-bold">IP Address Tracker</h1>
       <input
+        v-model="inputQuery"
+        @keyup.enter.native="getIpInfo"
         type="text"
-        class="mt-6 px-6 py-3 w-[35%] rounded-l-2xl focus:shadow focus:outline-none"
+        class="mt-8 mb-5 px-6 py-3 w-[35%] rounded-l-xl focus:shadow focus:outline-none"
         placeholder="Search any IP address or domain"
       />
-      <button class="bg-black text-white rounded-r-2xl px-5 py-3 font-bold">
+      <button
+        @click="getIpInfo"
+        class="bg-black text-white rounded-r-xl px-5 py-3 font-bold"
+      >
         >
       </button>
+
+      <span class="block text-red-500" v-if="error">
+        Domain or IP not found.
+      </span>
+
+      <section
+        class="bg-white mt-5 rounded-xl z-20 justify-around gap-5 py-5 md:p-6 grid md:grid-cols-4 relative max-w-5xl mx-auto shadow-xl text-left"
+      >
+        <div class="md:border-r-2 border-gray-200 pr-4">
+          <h4
+            class="text-gray-400 uppercase text-xs tracking-widest mb-3 font-medium"
+          >
+            IP Address
+          </h4>
+          <p class="text-xl font-medium">{{ ipInfo.ip }}</p>
+        </div>
+        <div class="md:border-r-2 border-gray-200 pr-4">
+          <h4
+            class="text-gray-400 uppercase text-xs tracking-widest mb-3 font-medium"
+          >
+            Location
+          </h4>
+          <p class="text-xl font-medium">
+            {{ ipInfo.city }}, {{ ipInfo.region }}, {{ ipInfo.country }}
+          </p>
+        </div>
+        <div class="md:border-r-2 border-gray-200 pr-4">
+          <h4
+            class="text-gray-400 uppercase text-xs tracking-widest mb-3 font-medium"
+          >
+            Timezone
+          </h4>
+          <p class="text-xl font-medium">UTC {{ ipInfo.timezone }}</p>
+        </div>
+        <div class="pr-4">
+          <h4
+            class="text-gray-400 uppercase text-xs tracking-widest mb-3 font-medium"
+          >
+            ISP
+          </h4>
+          <p class="text-xl font-medium">{{ ipInfo.isp }}</p>
+        </div>
+      </section>
     </div>
   </header>
-  <Map />
+  <div id="map" class="z-10"><div v-if="loading">Loading...</div></div>
 </template>
 
 <style>
@@ -30,5 +140,11 @@ import Map from './components/Map.vue'
 header {
   min-height: 250px;
   background-image: url('assets/pattern-bg.png');
+  background-position: center;
+  background-size: cover;
+}
+
+#map {
+  height: 78vh;
 }
 </style>
